ae61cb66d2288b5b7ee134d844710f540c230717
Robin <robin.bernard67@gmail.com>
2024-07-18 23:40:52 +0200

    caca

---
+++ tile_map.gd
@@ -0,0 +1,50 @@
+extends TileMap
+
+#grid variables
+const rows : int=14
+const cols : int=15
+const cell_size : int=50
+
+
+#tilemap variables
+var tile_id : int=2
+
+
+#layer variables
+var mine_layer : int=0
+var numbers_layer : int=1
+var grass_layer : int=2
+var flags_layer : int=3
+var hover_layer : int=4
+
+
+#atlas coordinates
+var mine_atlas := Vector2i(4, 0)
+
+#liste pour stocker les coords des mines
+var mine_coords := []
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+	
+
+func new_game():
+	clear()
+	mine_coords.clear()
+	generate_mines()
+
+func generate_mines():
+	for i in range(get_parent().total_mines):
+		var mine_pos = Vector2i(randi_range(0, cols - 1), randi_range(0, rows - 1))
+		mine_coords.append(mine_pos)
+		#add mine to tilemap
+		set_cell(mine_layer, mine_pos, tile_id, mine_atlas)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
