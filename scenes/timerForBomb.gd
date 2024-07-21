fea34ec6df4a37d306812ed78d7bd9f96a70fa30
Théo <157124427+Th2o1@users.noreply.github.com>
2024-07-21 19:36:59 +0200

    QTE touche prise en compte

--- scenes/timerForBomb.gd
+++ scenes/timerForBomb.gd
@@ -3,11 +3,13 @@ extends Node2D
 @onready var timer = $timer
 @onready var label = $timeRemaining
 @onready var keyToPress = $keyToPress
-var qte_keys = ["a", "z", "e", "r"]
+var qte_keys = ["A", "Z", "E", "R"]
 var sequence = []
 var current_index = 0
 var current_key = ""
 var countdown_time = 5  # Temps en secondes pour le compte à rebours
+
+
 
 func _ready():
 	timer.wait_time = 1
@@ -32,19 +34,35 @@ func start_timer():
 func start_qte():
 	generate_sequence(5)
 	current_index = 0
-	keyToPress.text = "Appuyez sur: " + current_key
-	show_next_key()
-	
-func show_next_key():
-	if current_index < sequence.size():
-		label.text = "Appuyez sur: " + sequence[current_index]
-	else:
-		qte_success()
+	keyToPress.text = "Appuyez sur: " + sequence[current_index]
 	
 func generate_sequence(length):
 	sequence.clear()
 	for i in range(length):
 		sequence.append(qte_keys[randi() % qte_keys.size()])
+		
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == get_scancode_from_key_string(sequence[current_index]):
+			current_index += 1
+			if current_index < sequence.size()-1:
+				keyToPress.text = "Appuyez sur: " + sequence[current_index]
+			else:
+				qte_success()
+		else:
+			print('neuille')
+			
+
 
 func qte_success():
-	pass
+	print('Win !')
+	timer.stop()
+
+#Helper 
+func get_scancode_from_key_string(key_string: String) -> int:
+	match key_string:
+		"A": return KEY_A
+		"Z": return KEY_Z
+		"E": return KEY_E
+		"R": return KEY_R
+		_: return -1
