1f0e7f41dc9235bb60bdae38c4deeb0aa8fc1b04
Théo <157124427+Th2o1@users.noreply.github.com>
2024-07-21 17:36:17 +0200

    Game Over and HUB

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,12 +3,20 @@ extends Node
 
 #games variables
 const total_mines : int=40
+var time_remaining : float
+var remaining_mines : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	new_game()
+	
+func new_game():
+	time_remaining = 120
+	remaining_mines = total_mines
+	$GameOver.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	time_remaining -= delta
+	$Hud.get_node("timeLabel").text = str(int(time_remaining))
+	$Hud.get_node("remainingBomb").text = str(remaining_mines)
